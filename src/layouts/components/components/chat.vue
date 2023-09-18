4679ec8a0b7b3335a0fdce31bc74efd517708fca
vvuoan <vvuoan@126.com>
2023-09-19 00:06:05 +0800

    modified:   src/layouts/components/components/chat.vue

--- src/layouts/components/components/chat.vue
+++ src/layouts/components/components/chat.vue
@@ -20,10 +20,22 @@
                 {{ message.text }}
             </div>
         </div>
-        <div class="h-[125px] bg-[#FBFBFB]">
-            <input v-model="inputText" type="text" placeholder="输入消息" />
-            <div class="">
-            <button @click="sendMessage" class="w-[72px] h-[32px] bg-[#FDCB9E] rounded-lg">发送</button>
+        <div class="h-[125px] bg-[#FBFBFB] rounded-3xl p-3">
+            <div class="Chat-input">
+                <input v-model="inputText" type="text" placeholder="这里输入..."/>
+            </div>
+            <div class="chat-function">
+                <div class="function">
+                    <div class="ml-2 flex justify-center items-center">
+                        <el-icon size="24px" color="#3F3F44"><Picture /></el-icon>
+                    </div>
+                    <div class="flex justify-center items-center">
+                        <el-icon size="24px" color="#3F3F44"><Link /></el-icon>
+                    </div>
+                </div>
+                <div class="ml-auto">
+                    <button @click="sendMessage" class="w-[72px] h-[32px] bg-[#FDCB9E] rounded-lg">发送</button>
+                </div>
             </div>
         </div>
     </div>
@@ -67,5 +79,23 @@ export default {
     .messagelist {
         width: 356px;
         height: calc(100% - 180px);
+    }
+    .Chat-input {
+        height: 70px;
+        @apply uppercase;
+
+    }
+    .Chat-input input {
+        width: 100%;
+        height: 70px;
+        outline:none;
+        background-color: #FBFBFB;
+    }
+    .chat-function {
+        height: 32px;
+        @apply flex justify-center items-center;
+    }
+    .function {
+        @apply flex justify-center items-center space-x-2;
     }
 </style>
